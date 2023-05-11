e8df069063302006a2ef031c63a327807dd1f217
Borhan uddin Amin <Borhanuddinamin@gmail.com>
2023-05-11 07:55:10 +0600

    project init

---
+++ Asp.net MVC CRUD with D.D.D/Asp.net MVC CRUD with D.D.D/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.net_MVC_CRUD_with_D.D.D.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Asp.net MVC CRUD with D.D.D/Asp.net MVC CRUD with D.D.D/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.net_MVC_CRUD_with_D.D.D.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
